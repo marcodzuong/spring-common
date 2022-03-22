a0ebe592d47b613e810020bcec234d0d6675ba65
bachdv <bachdv.hust@gmail.com>
2022-03-22 13:12:21 +0700

    init project

---
+++ src/test/java/com/bachdv/lib/SpringCommon/SpringCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachdv.lib.SpringCommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
