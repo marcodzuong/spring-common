01a24f4ccccf6b265bcf104c8aaf61f023b26d0a
Marco.Duong <marco.duong@techvify.com.vn>
2022-10-03 13:43:30 +0700

    update

--- src/main/java/com/bachdv/lib/common/task/model/BaseTask.java
+++
@@ -1,9 +0,0 @@
-package com.bachdv.lib.common.task.model;
-
-/**
- * Implement detail if want to manage in db
- * @author Marco.Duong
- */
-public interface BaseTask {
-
-}
--- src/main/java/com/bachdv/lib/common/task/service/JobService.java
+++ src/main/java/com/bachdv/lib/common/task/service/JobService.java
@@ -1,13 +1,12 @@
 package com.bachdv.lib.common.task.service;
 
-import com.bachdv.lib.common.task.model.BaseTask;
 import com.bachdv.lib.common.task.model.QuartzJobDetail;
 import org.quartz.SchedulerException;
 
 /**
  * @author Marco.Duong
  */
-public interface JobService<TASK extends BaseTask> {
+public interface JobService {
 
     boolean addJob(QuartzJobDetail job) throws SchedulerException;
 
